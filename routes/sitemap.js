f2c36966aad2cf4b7b4f19a36be3b310d81a264c
Suhee231231 <147281452+Suhee231231@users.noreply.github.com>
2025-08-21 03:00:53 +0900

    사이트맵 테스트 버전 추가 - 배포 확인용

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -14,6 +14,7 @@ router.get('/', async (req, res) => {
         
         // 정적 페이지들
         let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<!-- 🚀 NEW SITEMAP VERSION - ${currentDate} - SEO OPTIMIZED -->
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>${baseUrl}/</loc>
